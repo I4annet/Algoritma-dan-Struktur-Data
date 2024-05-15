147f80841f7fb6653b2ce5d8466fc9469c62c6d4
Ivansyah <ivansyahsantoso@gmail.com>
2024-05-15 11:14:00 +0700

    update latihan

---
+++ Tugas11/Latihan/Node.java
@@ -0,0 +1,13 @@
+package Tugas11.Latihan;
+
+public class Node {
+    int nim;
+    String nama;
+    Node next;
+
+    Node(int nim, String nama, Node berikutnya) {
+        nim = nim;
+        nama = nama;
+        next = berikutnya;
+    }
+}
--- Tugas11/Latihan/SIngleLinkedlistMhs.java
+++ Tugas11/Latihan/SIngleLinkedlistMhs.java
@@ -2,4 +2,86 @@ package Tugas11.Latihan;
 
 public class SIngleLinkedlistMhs {
     Node head, tail;
+
+boolean isEmpty() {
+    return head == null;
 }
+
+void print() {
+    if (!isEmpty()) {
+        Node tmp = head;
+        System.out.print("Isi Linked list: ");
+        while (tmp != null) {
+            System.out.println(tmp.nim + "\t");
+            tmp = tmp.next;
+        }
+        System.out.println();
+    } else {
+        System.out.println("Linked list dalam keadaan kosong");
+    }
+}
+
+void addFirst(int input) {
+    Node ndInput = new Node(input, null, head);
+    if (isEmpty()) {
+        head = ndInput;
+        tail = ndInput;
+        } else {
+            ndInput.next = head;
+            head = ndInput;
+        }
+    }
+
+void addLast(int input) {
+    Node ndInput = new Node(input, null, head);
+    if (isEmpty()) {
+        head = ndInput;
+        tail = ndInput;
+    } else {
+        tail.next = ndInput;
+        tail = ndInput;
+    }
+}
+
+ void insertAfter(int key, int input) {
+    Node ndInput = new Node(input, null, head);
+    Node temp = head;
+    while (temp != null) {
+        if (temp.nim == key) {
+            ndInput.next = temp.next;
+            temp.next = ndInput;
+            if (ndInput.next == null) {
+                tail = ndInput;
+            }
+            return;
+        }
+        temp = temp.next;
+    }
+    System.out.println("Tidak ada di list");
+ }
+
+ void insertAt(int index, int input) {
+    Node ndInput = new Node(input, null, head);
+    if (index < 0) {
+        System.out.println("Di perbaiki");
+    } if (index == 0) {
+        addFirst(input);
+    } else {
+        Node temp = head;
+        for (int i = 0; temp != null && i < index - 1; i++) {
+            temp = temp.next;
+        }
+        if (temp == null) {
+            System.out.println("Index terlalu banyak");
+            return;
+        }
+        ndInput.next = temp.next;
+        temp.next = ndInput;
+        if (temp.next.next == null) {
+            tail = temp.next;
+        }
+    }
+ }
+
+}
+
